8427c32aa42145fb5200046e5a39025b63fbe691
Jxiang-jc <zjx1020645@163.com>
2019-01-24 23:42:57 +0800

    完成 webpack.config.dev 的配置

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require("webpack");
+const path = require('path')
+module.exports = {
+    entry: ["./src/javascript/app.js"],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "bundle.js"
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.BannerPlugin("这里是打包文件头部注释")
+    ]
+}
